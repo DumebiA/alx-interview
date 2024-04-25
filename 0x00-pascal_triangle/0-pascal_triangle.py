682a93ed569b5abe6aeb836a3bd3c6eb09bd6916
root <anyamkpa.ifeanyi@gmail.com>
2024-04-25 09:15:49 -0700

    considered if less than 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
+"""
+0. Pascal's Triangle
+"""
+
 
 def pascal_triangle(n):
+    """Generates Pascal's triangle of size n
     """
-    Generates Pascal's triangle of size n
-    """
-    if n <= 0:
-        return []
-
-    triangle = [[1]]
-
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]  # First element of each row is always 1
-
-        # Calculate the elements in between using the previous row
-        for j in range(1, i):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-
-        new_row.append(1)  # Last element of each row is always 1
-        triangle.append(new_row)
-
+    triangle = []
+    if n > 0:
+        for i in range(1, n + 1):
+            newLevel = []
+            D = 1
+            for j in range(1, i + 1):
+                newLevel.append(D)
+                D = D * (i - j) // j
+            triangle.append(newLevel)
     return triangle
